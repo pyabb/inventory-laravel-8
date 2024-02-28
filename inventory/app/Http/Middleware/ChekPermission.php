4194746a4a2070666a2080b449402943dbd5e55d
pyabb <raulqb.89@gmail.com>
2024-02-28 05:23:23 -0500

    cbdi - fix: Enable permission check functionality in middleware
    Uncommented previously disabled permission check functionality in middleware. With this change, the system will now validate user permissions based on their role and restrict access to certain routes accordingly. Unauthorized access will now result in a 404 error response.

--- inventory/app/Http/Middleware/ChekPermission.php
+++ inventory/app/Http/Middleware/ChekPermission.php
@@ -17,28 +17,35 @@ class ChekPermission
      */
     public function handle($request, Closure $next)
     {
-//        if(Auth::check()){
-//         $role_id = Auth::user()->role_id;
-//            $namedRoute 		= \Route::currentRouteName();
-//            $current_url_check  = DB::table('menus')->select('menu_url')->where('menu_url', $namedRoute)->get()->toArray();
-//            if ($namedRoute)
-//            {
-//                if ($current_url_check)
-//                {
-//                    $permissionCheck = DB::table('menus')
-//                        ->join('permissions', 'permissions.menu_id', '=', 'menus.id')
-//                        ->where('role_id', $role_id)
-//                        ->where('menu_url', $namedRoute)
-//                        ->get()->toArray();
-//                    if (empty($permissionCheck) || count($permissionCheck) <= 0)
-//                    {
-//                        return response()->view('errors.404', [], 404);
-//                    }
-//                }
-//            }
-//
-//         }
+        if(Auth::check())
+        {
+            $role_id = Auth::user()->role_id;
+            $namedRoute = \Route::currentRouteName();
 
+            $current_url_check  = DB::table('menus')
+                ->select('menu_url')
+                ->where('menu_url', $namedRoute)
+                ->get()
+                ->toArray();
+
+            if ($namedRoute)
+            {
+                if ($current_url_check)
+                {
+                    $permissionCheck = DB::table('menus')
+                        ->join('permissions', 'permissions.menu_id', '=', 'menus.id')
+                        ->where('role_id', $role_id)
+                        ->where('menu_url', $namedRoute)
+                        ->get()->toArray();
+
+                    if (empty($permissionCheck) || count($permissionCheck) <= 0)
+                    {
+                        return response()->view('errors.404', [], 404);
+                    }
+                }
+            }
+
+         }
 
         return $next($request);
     }
