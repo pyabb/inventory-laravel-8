fe14a7666a5e27fa80510666323441de1290d984
pyabb <raulqb.89@gmail.com>
2025-02-03 18:48:11 -0500

    feat: Update dependencies and structure to migrate from laravel 5.6 to 5.7

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,15 +11,15 @@ let mix = require("laravel-mix");
  |
  */
 
-mix.js("resources/assets/js/app.js", "public/js");
-mix.js("resources/assets/js/vendor.js", "public/js");
-mix.js("resources/assets/js/product.js", "public/js");
-mix.js("resources/assets/js/stock.js", "public/js");
-mix.js("resources/assets/js/category.js", "public/js");
-mix.js("resources/assets/js/invoice.js", "public/js");
-mix.js("resources/assets/js/report.js", "public/js");
-mix.js("resources/assets/js/role.js", "public/js");
-mix.js("resources/assets/js/user.js", "public/js");
-mix.js("resources/assets/js/customer.js", "public/js");
-mix.js("resources/assets/js/dashboard.js", "public/js");
-//mix.sass("resources/assets/sass/app.scss", "public/css");
+mix.js("resources/js/app.js", "public/js");
+mix.js("resources/js/vendor.js", "public/js");
+mix.js("resources/js/product.js", "public/js");
+mix.js("resources/js/stock.js", "public/js");
+mix.js("resources/js/category.js", "public/js");
+mix.js("resources/js/invoice.js", "public/js");
+mix.js("resources/js/report.js", "public/js");
+mix.js("resources/js/role.js", "public/js");
+mix.js("resources/js/user.js", "public/js");
+mix.js("resources/js/customer.js", "public/js");
+mix.js("resources/js/dashboard.js", "public/js");
+//mix.sass("resources/sass/app.scss", "public/css");
